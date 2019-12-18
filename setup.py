2b21b269b8e90662440a8e76a425f6a20b71e40a
Petar <kenicpetar@yahoo.com>
2019-12-18 09:58:12 -0500

    added ldraw library locally

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from distutils.core import setup
+from setuptools import find_packages
+import codecs
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name = 'StarkLego',         # How you named your package folder (MyLib)
-    packages = ['StarkLego'],   # Chose the same as "name"
-    version = '0.1.1',      # Start with a small number and increase it with every change you make
+    packages=find_packages(),
+    version = '0.1.2',      # Start with a small number and increase it with every change you make
     license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description = 'Library used to create lego builds',   # Give a short description about your library
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author = 'Petar Kenic',                   # Type in your name
     author_email = 'kenicpetar@yahoo.com',      # Type in your E-Mail
     url = 'https://github.com/peken97/StarkLego',   # Provide either the link to your github or to your website
-    download_url = 'https://github.com/peken97/StarkLego/archive/v0.1.1.tar.gz',    # I explain this later on
     keywords = ['LEGO', 'BUILD', 'LDRAW'],   # Keywords that define your package best
     install_requires=[            # I get to this in a second
             'gym',
