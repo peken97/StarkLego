4452ba9686e2e3814876359cf05006192f4187dc
Petar K <kenicpetar@yahoo.com>
2019-12-29 16:39:56 -0500

    Cleaned up files that I don't want and added it to .gitignore

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 setup(
     name = 'StarkLego',         # How you named your package folder (MyLib)
     packages=find_packages(),
-    version = '0.1.2',      # Start with a small number and increase it with every change you make
+    version = '0.1.6',      # Start with a small number and increase it with every change you make
     license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description = 'Library used to create lego builds',   # Give a short description about your library
     long_description=long_description,
@@ -20,7 +20,8 @@ setup(
     install_requires=[            # I get to this in a second
             'gym',
             'pyldraw',
-            'numpy',
+            'numpy==1.16.0',
+            'tensorflow==1.8.0'
       ],
     classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
